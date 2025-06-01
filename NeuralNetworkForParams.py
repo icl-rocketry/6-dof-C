f690b8a1362f822dbaea77610944b580dfe2f02c
youngers2006 <sy2224@ic.ac.uk>
2025-06-01 21:53:10 +0100

    Update NeuralNetworkForParams.py

--- NeuralNetworkForParams.py
+++ NeuralNetworkForParams.py
@@ -10,11 +10,11 @@ class param_approx_boattail_NN(nn.module): # NN to approximate each part paramet
         self.layer2 = nn.Linear(128,128)
         self.layer3 = nn.Linear(128,64)
         self.layer4 = nn.Linear(64,out_dim)
-        def forward(self,x):
-            x = torch.relu(self.layer1(x))
-            x = torch.relu(self.layer2(x))
-            x = torch.relu(self.layer3(x))
-            return self.layer4(x)
+    def forward_pass(self,x):
+        x = torch.relu(self.layer1(x))
+        x = torch.relu(self.layer2(x))
+        x = torch.relu(self.layer3(x))
+        return self.layer4(x)
     
 
     
